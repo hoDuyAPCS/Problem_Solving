1756c2889e8e6aaceeb7b6b68f9436a538dc7b3b
Ho Duy <40297016+hoDuyAPCS@users.noreply.github.com>
2019-11-23 14:36:09 +0700

    Update P4_CPP.cpp

--- Project_Euler/P4_CPP.cpp
+++ Project_Euler/P4_CPP.cpp
@@ -1,7 +1,6 @@
 //Largest Palindrome made from product of two 3-digit number
 #include <iostream>
-#include <string>
-#include <string.h>
+#include <math.h>
 using namespace std;
 /*
 Algo1:
@@ -20,17 +19,20 @@ Algo3:
 
 Algo4:
 1. Recursion?
-*/
 
+Algo5: Try from the biggest palindrome, find out if we can factor it into the product of two 3 digits number
+*/
+//Reference - useless
 bool checkPalindrome(int num){
   string number_string = to_string(num);
   int number_index = strlen(number_string)-1;
-  for (int i = 0; i++; i*2 <= number_index){
+  for (int i = 0;i*2 <= number_index ;i++ ){
     if (number_string[i] != number_string[number_index-i]) return false;
   }
   return true;
 }
 
+//Reference - useless
 long int findThePalindrome(int alpha, int beta){
   if (checkPalindrome(alpha*beta)) return alpha*beta;
   else{
@@ -43,6 +45,30 @@ long int findThePalindrome(int alpha, int beta){
 }
 
 int main(){
-  cout << findThePalindrome(999,999);
-  return 0;
+  //I test case-by-case
+  //case 1 : palindrome with 6 digit => I got the correct answer luckily
+	int palindrome = 999999; //maximum palindrome with 6 digits
+  //when palindrome == 100001
+	while(palindrome-100000!=1){
+    //i is the divisor for palindrome
+		for(int i=100;i<=sqrt(palindrome);i++){
+			if (palindrome%i==0){
+        //if the quotient has 3 digits
+				if (palindrome/i < 1000) {
+          //RESULT
+          cout << palindrome << " : " << i << " X " << palindrome/i <<endl;
+          return 0;
+        }
+      }
+    }
+    //990099 -> 989989 -> 988889
+		if ((palindrome%1000)/100==0) {
+      //900009 -> 899998 -> 898898 
+			if ((palindrome%100)/10==0) palindrome -= 11;
+			else palindrome -= 110;
+		}
+    //998899 -> 997799 -> 996699
+		else palindrome -= 1100;
+	}
+	return 0;
 }
