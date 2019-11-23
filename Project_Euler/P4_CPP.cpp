7a2d4dac97fe64bede0ceff7f3828d59df7ad46c
Ho Duy <40297016+hoDuyAPCS@users.noreply.github.com>
2019-11-23 11:55:01 +0700

    Update P4_CPP.cpp

--- Project_Euler/P4_CPP.cpp
+++ Project_Euler/P4_CPP.cpp
@@ -1,6 +1,7 @@
 //Largest Palindrome made from product of two 3-digit number
 #include <iostream>
 #include <string>
+#include <string.h>
 using namespace std;
 /*
 Algo1:
@@ -13,6 +14,12 @@ Algo1:
 Algo2:
 1. Try every combination of number 999.999 to 100.100
 2. Get max_palindrome to store the largest palindrome
+
+Algo3:
+1. Try from the biggest product of 2 3 digits number 
+
+Algo4:
+1. Recursion?
 */
 
 bool checkPalindrome(int num){
@@ -22,4 +29,20 @@ bool checkPalindrome(int num){
     if (number_string[i] != number_string[number_index-i]) return false;
   }
   return true;
+}
+
+long int findThePalindrome(int alpha, int beta){
+  if (checkPalindrome(alpha*beta)) return alpha*beta;
+  else{
+    if (alpha == beta) {
+      alpha = 999;beta -= 1;
+      return findThePalindrome(alpha,beta);
+    }
+    else return findThe Palindrome(alpha-1,beta);
+  }
+}
+
+int main(){
+  cout << findThePalindrome(999,999);
+  return 0;
 }
